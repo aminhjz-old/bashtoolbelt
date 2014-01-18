ddeb593a4d0d2a1714cfbb9a997179dc738d5c94
amin <amin.hedjazi@gmail.com>
2014-01-18 11:30:45 +0330

    added script to decompile a jar copletely

--- decompile.sh
+++ decompile.sh
@@ -1,8 +1,19 @@
+folder=`basename $1 .jar`
+mkdir $folder
+cd $folder
+unzip ../$1
 
 find . -name "*.class" | while read file
 do
+    sudo chown amin.amin $file
+    chmod 775 $file
     class=`basename $file .class`
     out_file=$(dirname $file)/${class}.java
     echo "Decompiling $class"
     jad -p $file > $out_file
+    rm $file    
 done
+cd ../
+chmod 775 $folder -R
+jar cf $folder.src.jar $folder
+rm $folder -rf
